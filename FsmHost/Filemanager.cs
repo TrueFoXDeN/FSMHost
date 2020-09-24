c054e291e57f04f04a9434d8555055cab951b07c
Laurenz <laurenz.dilba@hotmail.de>
2020-09-24 16:07:50 +0200

    Added ban / unban and console commands

---
+++ FsmHost/Filemanager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FsmHost
+{
+    class Filemanager
+    {
+
+        public static string blacklist(string user, string ip)
+        {
+            Boolean alreadyBanned = false;
+            var lines = File.ReadAllLines("blacklist.txt");
+            foreach (string s in lines)
+            {
+                alreadyBanned = alreadyBanned == false ? user != "" ? s.Contains(user) : ip != "" ? s.Contains(ip) : false : false;
+            }
+            if (alreadyBanned)
+            {
+                return $"{user} has already been banned from the server.";
+            }
+            File.AppendAllText("blacklist.txt", $"ban;{user};{ip}\n");
+            return $"{user} has been banned from the server.";
+        }
+        public static string removeFromBlacklist(string user, string ip)
+        {
+            if (File.Exists("blacklist.txt"))
+            {
+                File.WriteAllLines("blacklist.txt", File.ReadLines("blacklist.txt")
+                    .Where(l => !(user != "" ? l.Contains(user) : ip != "" ? l.Contains(ip) : false)).ToList());
+            }
+            return $"{user} has been unbanned from the server.";
+                
+        }
+
+    }
+}
--- FsmHost/Program.cs
+++ FsmHost/Program.cs
@@ -26,36 +26,51 @@ namespace FsmHost
 
         private static void SetupServer()
         {
-            string changePort = string.Empty;
+            char changePort = 'x';
             int port = 13000;
             string stringPort = string.Empty;
             Console.Title = "FlightStrip Manager Server";
             Console.WriteLine("Setup Server:");
             Console.WriteLine("Standard port is set to " + port + ".");
-            Console.Write("Do you want to use a custom port? (y/n): ");
-            changePort = Console.ReadLine();
-            if (changePort.ToLower() == "y")
-            {
-                Console.Write("Desired port: ");
-                stringPort = Console.ReadLine();
-                if (stringPort != "")
-                {
-                    try
-                    {
-                        port = Int32.Parse(stringPort);
-                        Console.WriteLine("Port set to " + port);
-                    }
-                    catch (FormatException e)
-                    {
-                        Console.WriteLine("Input wasn't a number. Port set to 13000");
-                    }
 
-                }
-            }
-            else
+
+            while (!(changePort == 'y' || changePort == 'n'))
             {
-                Console.WriteLine("Port set to " + port);
+                Console.Write("Do you want to use a custom port? (y/n): ");
+                changePort = Console.ReadKey().KeyChar;
+
+                if (changePort == 'y')
+                {
+                    Console.WriteLine();
+                    Console.Write("Desired port: ");
+                    stringPort = Console.ReadLine();
+                    if (stringPort != "")
+                    {
+                        try
+                        {
+                            port = Int32.Parse(stringPort);
+                            Console.WriteLine("Port set to " + port);
+                        }
+                        catch (FormatException e)
+                        {
+                            Console.WriteLine("Input wasn't a number. Port set to 13000");
+                        }
+
+                    }
+                }
+                else if (changePort == 'n')
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Port set to " + port);
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+
             }
+
+
 
 
             Console.WriteLine("Setting up server...");
@@ -78,11 +93,60 @@ namespace FsmHost
                     }
                 }
                 Console.WriteLine();
+                Console.WriteLine("To list all commands, type: help");
                 Console.WriteLine("Setup finished. Server started at " + DateTime.Now.ToLongTimeString());
                 Console.WriteLine("---------------------------------------------");
                 while (true)
                 {
-                    Console.ReadLine();
+                    string line = Console.ReadLine();
+                    if (!String.IsNullOrWhiteSpace(line))
+                    {
+                        string[] commands = line.Split(' ');
+                        switch (commands[0])
+                        {
+                            case "help":
+                                Console.WriteLine("kick         Enter kick followed by the username to force a disconnect on the specified user.");
+                                Console.WriteLine("ban          Enter ban followed by the username to permanently ban a specified user.");
+                                Console.WriteLine("banip        Enter ban followed by the username to permanently ban a specified user.\n" +
+                                    "             This option blocks every account from connecting, coming from this ip");
+                                Console.WriteLine("unban        Enter unban followed by the username to unban a specified user.");
+                                Console.WriteLine("unbanip      Enter unbanip followed by the ip adress to unban a specified ip adresss.");
+                                Console.WriteLine("usewhitelist Enter usewhitelist followed by true or false to enable or disable the whitelist.\n" +
+                                    "             If this option is enabled, only usernames in the textfile \"whitelist.txt\" will be allowed to connect.");
+                                break;
+                            case "ban":
+                                
+                                if (commands.Length == 2)
+                                {
+                                    
+                                    Console.WriteLine(Filemanager.blacklist(commands[1], ""));
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Wrong number of arguments");
+                                }
+
+                                break;
+                            case "banip":
+                                break;
+                            case "unban":
+                                if (commands.Length == 2)
+                                {
+                                    Console.WriteLine(Filemanager.removeFromBlacklist(commands[1], ""));
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Wrong number of arguments");
+                                }
+                                break;
+                            case "unbanip":
+                                break;
+
+                            default:
+                                Console.WriteLine("No valid command entered. Type help to list all commands.");
+                                break;
+                        }
+                    }
                 }
             }
             catch (SocketException e)
@@ -94,8 +158,8 @@ namespace FsmHost
 
         public static void dataReceived(object sender, Message e)
         {
-            
-          
+
+
             Debug.WriteLine(e.MessageString);
             manager.processReceivedData(e.MessageString, e);
         }
